f56a38188f9c7d242d17fc2c062e139c63fa160c
Francisco Lopes da Silva Júnior <juniorflsjr@gmail.com>
2023-05-12 17:05:57 -0300

    atualização do  projeto

--- js/validacao.js
+++ js/validacao.js
@@ -1,21 +1,49 @@
 export function valida(input) {
     const tipoDeInput = input.dataset.tipo
 
-    if (validadores[tipoDeUnput]) {
+
+    if (validadores[tipoDeInput]) {
         validadores[tipoDeInput](input)
+    }
+    
+    if(input.validity.valid) {
+        input.parentElement.classList.remove("input-container--invalido")
+     }else {
+         input.parentElement.classList.add("input-containerr--invalido")
+     }
+ 
+}
+
+const mensagensDeErro = {
+    nome: {
+        valueMissing: 'O campo nome não pode estar vazio.'
+    },
+    email:{
+        valueMissing: 'O campo de email não pode estar vazio.',
+        typeMismatch: 'O email digitado não é válido.'
+    },
+    senha: {
+        valueMissing: 'O campo de senha não pode estar vazio',
+        patternMismatch: 'Sua senha deve conter entre 6 a 12 caracters, deve conter pelo menos uma          letra  maiúscula, um número e não deve conter símbolos.'
+    },
+    dataNacimento: {
+        valueMissing: 'O campo de data de nacimento não pode estar vazio.',
+        customError: 'Você deve ser maior que 18 anos para se cadastrar.'
     }
 }
 
+
+
 const validadores = {
-    dataNacimento:input => validaDataNascimento(input)
+    dataNacimento:input => validaDataNacimento(input)
 }
 
-function validaDataNacimente(input){
+function validaDataNacimento(input){
     const dataRecebida = new Date(input.value)
     let mensagem = ''
 
     if (!maiorQue18(dataRecebida)) {
-    mensagem = 'Você deve ser maior que 18 anos para se cadastras.'
+    mensagem = 'Você deve ser maior que 18 anos para se cadastrar.'
     }
 
     input.setCustomValidity(mensagem)
